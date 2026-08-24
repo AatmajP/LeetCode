c3a6c404a7b1541a3a14d48650d972802c133d54
AatmajP <167125648+AatmajP@users.noreply.github.com>
2026-08-24 18:08:59 +0530

    Time: 0 ms (100%), Space: 42.1 MB (59.12%) - LeetHub

---
+++ 0556-next-greater-element-iii/0556-next-greater-element-iii.java
@@ -0,0 +1,43 @@
+
+
+class Solution {
+    public int nextGreaterElement(int n) {
+
+        char[] arr = String.valueOf(n).toCharArray();
+
+        
+        int i = arr.length - 2;
+
+        while (i >= 0 && arr[i] >= arr[i + 1]) {
+            i--;
+        }
+
+        if (i < 0) {
+            return -1;
+        }
+
+  
+        int j = arr.length - 1;
+
+        while (arr[j] <= arr[i]) {
+            j--;
+        }
+
+       
+        char temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+
+        
+        Arrays.sort(arr, i + 1, arr.length);
+
+ 
+        long result = Long.parseLong(new String(arr));
+
+        if (result > Integer.MAX_VALUE) {
+            return -1;
+        }
+
+        return (int) result;
+    }
+}
