60ed61c779149023c48c5446f5f74a501dc0c1d3
AatmajP <167125648+AatmajP@users.noreply.github.com>
2026-09-04 13:38:36 +0530

    Time: 4 ms (100%), Space: 46.7 MB (32.28%) - LeetHub

---
+++ 0622-design-circular-queue/0622-design-circular-queue.java
@@ -0,0 +1,55 @@
+class MyCircularQueue {
+
+    int[] arr;
+    int front;
+    int rear;
+    int size;
+    int k;
+
+    public MyCircularQueue(int k) {
+        this.k = k;
+        arr = new int[k];
+        front = 0;
+        rear = -1;
+        size = 0;
+    }
+
+    public boolean enQueue(int value) {
+        if(size == k) return false;
+
+        rear = (rear + 1) % k;
+        arr[rear] = value;
+        size++;
+
+        return true;
+    }
+
+    public boolean deQueue() {
+        if(size == 0) return false;
+
+        front = (front + 1) % k;
+        size--;
+
+        return true;
+    }
+
+    public int Front() {
+        if(isEmpty()) return -1;
+
+        return arr[front];
+    }
+
+    public int Rear() {
+        if(isEmpty()) return -1;
+
+        return arr[rear];
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public boolean isFull() {
+        return size == k;
+    }
+}
