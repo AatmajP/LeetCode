645105ff42216fcad02bc081ddcbd747694a04c3
AatmajP <167125648+AatmajP@users.noreply.github.com>
2026-09-17 20:04:30 +0530

    Time: 3 ms (99.8%), Space: 47.1 MB (64.34%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,4 +1,4 @@
-class Solution {
+/*class Solution {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
 
         ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
@@ -41,5 +41,49 @@ class Solution {
         }
 
         return count == numCourses;
+    }
+}*/
+
+// this is dfs method
+
+class Solution {
+    ArrayList<ArrayList<Integer>> graph;
+    int[] state;
+    boolean dfs(int node){
+        state[node]=1; 
+        for(int nei:graph.get(node)){
+            if(state[nei]==1) return false;
+            if(state[nei]==0){
+                if(!dfs(nei)) return false;
+            }
+        }
+        state[node]=2;
+        return true;
+    }
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        graph=new ArrayList<>();
+        for(int i=0;i<numCourses;i++){
+            graph.add(new ArrayList<>());
+        }
+        for(int[] pre:prerequisites){
+            int a=pre[0];
+            int b=pre[1];
+            graph.get(b).add(a);
+
+        }
+         state = new int[numCourses];
+
+        // Check every component
+        for (int i = 0; i < numCourses; i++) {
+
+            if (state[i] == 0) {
+
+                if (!dfs(i)) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
     }
 }
